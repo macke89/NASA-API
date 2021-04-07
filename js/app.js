e2123bdcc481e83331003753f6a71f37be85c852
marko <mark.opp.1989@gmail.com>
2021-04-07 11:59:03 +0900

    Secure API_KEY

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
+"use strict";
+
+let API_KEY = "abq6kLKrCDibA3obJuRVt3phoqie3msWIj3XfB4f";
+
 let searchButton = document.querySelector("#search");
 
 searchButton.addEventListener("click", () => {
-  console.log("button pressed");
+  // console.log("button pressed");
   sendApiRequest();
 });
 
 async function sendApiRequest() {
-  let response = await fetch(``);
-  console.log(response);
+  let response = await fetch(
+    `https://api.nasa.gov/planetary/earth/imagery?lon=-220.090083&lat=35.661358&dim=0.3&api_key=${API_KEY}`
+  );
+  // let data = await response.json();
+  // console.log(data);
+  // useApiData(data);
 }
 
-function useApiData(data) {}
+function useApiData(data) {
+  // document.querySelector("#content").innerHTML += `<img src='${data}'>`;
+  document.querySelector("#content").innerHTML = "test";
+}
