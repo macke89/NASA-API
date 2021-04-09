005af477cb6ed1875d8fe7398274cd527e43cae5
marko <mark.opp.1989@gmail.com>
2021-04-09 22:35:37 +0900

    Design / Responsive

--- js/app.js
+++ js/app.js
@@ -1,24 +1,53 @@
 "use strict";
 
-let API_KEY = "abq6kLKrCDibA3obJuRVt3phoqie3msWIj3XfB4f";
+// POPUPS
+const popups = document.querySelectorAll(".popup");
+const modal = document.querySelector(".popup-window");
+console.log(popups);
+console.log(modal);
 
-let searchButton = document.querySelector("#search");
-
-searchButton.addEventListener("click", () => {
-  // console.log("button pressed");
-  sendApiRequest();
+popups.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    console.log("open");
+    modal.classList.toggle("-inset-x-full");
+    modal.classList.toggle("opacity-0");
+    let url = event.target.src;
+    modal.innerHTML = `
+      <img class="popup-image object-contain inset-center w-full h-full popup-close cursor-pointer transition-all duration-1000" src="${url}" />
+    `;
+    const close = document.querySelector(".popup-close");
+    close.addEventListener("click", function () {
+      modal.classList.toggle("opacity-0");
+      window.setTimeout(function () {
+        modal.classList.toggle("-inset-x-full");
+      }, 700);
+      console.log("close");
+      modal.innerHTML = "";
+    });
+  });
 });
 
-async function sendApiRequest() {
-  let response = await fetch(
-    `https://api.nasa.gov/planetary/earth/imagery?lon=-220.090083&lat=35.661358&dim=0.3&api_key=${API_KEY}`
-  );
-  // let data = await response.json();
-  // console.log(data);
-  // useApiData(data);
-}
+new Glide(".glide").mount();
 
-function useApiData(data) {
-  // document.querySelector("#content").innerHTML += `<img src='${data}'>`;
-  document.querySelector("#content").innerHTML = "test";
-}
+// let API_KEY = "abq6kLKrCDibA3obJuRVt3phoqie3msWIj3XfB4f";
+
+// let searchButton = document.querySelector("#search");
+
+// searchButton.addEventListener("click", () => {
+//   // console.log("button pressed");
+//   sendApiRequest();
+// });
+
+// async function sendApiRequest() {
+//   let response = await fetch(
+//     `https://api.nasa.gov/planetary/earth/imagery?lon=-220.090083&lat=35.661358&dim=0.3&api_key=${API_KEY}`
+//   );
+//   // let data = await response.json();
+//   // console.log(data);
+//   // useApiData(data);
+// }
+
+// function useApiData(data) {
+//   // document.querySelector("#content").innerHTML += `<img src='${data}'>`;
+//   document.querySelector("#content").innerHTML = "test";
+// }
